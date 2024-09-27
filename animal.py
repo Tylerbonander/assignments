b12cde5071ebeadce3faee9705929f2efa1c3f92
Tylerbonander <bonandet@oregonstate.edu>
2024-09-27 14:22:04 -0700

    backup

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Tyler Bonander
+# GitHub: Tylerbonander
+# Date: 09/27/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+fave_animal = input("please enter your favorite animal: ")
+print("your favorite animal is " + fave_animal + ".")
